5fb75e9b9ea847178d03013ca1324c533c73b7b3
Damla Koksal <me@damlakoksal.com>
2021-10-25 19:36:46 +0300

    [#15] feat: connect backend to Redis found on Heroku

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
-
+import client from "./redis.js";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.end("Search App Backend");
+app.get("/", async (req, res) => {
+  await client.set("name", "damla");
+  const name = await client.get("name");
+  res.end(`name value: ${name}`);
 });
 
 app.listen(PORT, () => {
