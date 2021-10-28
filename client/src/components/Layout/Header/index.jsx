9e0030951744c99f59383b85a7771e7a0bdae93d
Damla Koksal <me@damlakoksal.com>
2021-10-28 22:45:29 +0300

    [#2] feat: make Header part pixel perfect

--- client/src/components/Layout/Header/index.jsx
+++ client/src/components/Layout/Header/index.jsx
@@ -4,8 +4,14 @@ import { Search, Basket } from "../..";
 export default function Header() {
   return (
     <header className={styles.Container}>
-      <a href="/" target="" title="Hepsiburada">
-        <img className={styles.Logo} src={Logo} alt="hb-logo" />
+      <a className={styles.Link} href="/" target="" title="Hepsiburada">
+        <img
+          className={styles.Logo}
+          src={Logo}
+          width="211.52"
+          height="35"
+          alt="hb-logo"
+        />
       </a>
       <Search />
       <Basket />
