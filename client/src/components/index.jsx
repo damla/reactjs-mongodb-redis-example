52aca813df5e22964755a66be422d40130263f0c
Damla Koksal <me@damlakoksal.com>
2021-10-28 14:44:26 +0300

    [#2] feat: Implement Header component

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -3,3 +3,4 @@ export { default as Header } from "./Layout/Header";
 export { default as Section } from "./Layout/Section";
 export { default as Sidebar } from "./Layout/Sidebar";
 export { default as SubHeader } from "./Layout/SubHeader";
+export { default as Search } from "./Search";
