34d8efed0ad063312f746f6120129ca116b36738
Damla Koksal <me@damlakoksal.com>
2021-11-01 07:12:27 +0300

    [#8] feat: init test files

--- client/src/components/AddButton/index.test.jsx
+++ client/src/components/AddButton/index.test.jsx
@@ -1,39 +1,18 @@
-// import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import { render } from "@testing-library/react";
+import { ReactDOM } from "react";
+import AddButton from "./index";
 
-// import AddButton from "./index";
+it("Expect render without crash.", () => {
+  const div = document.createElement("div");
+  const product = [{ name: "test", brand: "apple", _id: 6161, price: 61.99 }];
+  const addItem = (product) => console.log(product);
 
-// it("Ekleme tuşuna tıklandığında tuş deaktive olur.", () => {
-//   let disabled_add_btn;
+  render(
+    <AddButton
+      onClick={() => {
+        addItem(product[0]);
+      }}
+    />
+  );
+});
 
-//   render(<AddButton />);
-
-//   disabled_add_btn = screen.getByTestId("disabled-add-btn");
-
-//   setTimeout(() => {
-//     expect(screen.getByTestId("result")).. });
-// });
-
-// it("renders welcome message", () => {
-//   render(<App />);
-//   expect(screen.getByText("Learn React")).toBeInTheDocument();
-// });
-
-// // it("Ekleme tuşuna tıklandığında local storage üzerinden veriyi kaydeder.", () => {
-// //   let number1_input, number2_input, submit_btn;
-
-// //   render(<Calculator />);
-
-// //   number1_input = screen.getByTestId("number1");
-// //   number2_input = screen.getByTestId("number2");
-// //   submit_btn = screen.getByTestId("submit-btn");
-
-// //   userEvent.type(number1_input, 4);
-// //   userEvent.type(number2_input, 4);
-
-// //   userEvent.click(submit_btn);
-
-// //   setTimeout(() => {
-// //     expect(screen.getByTestId("result")).toHaveTextContent(16);
-// //   });
-// // });
